5c09f827ed155199813c1b117dc4095a00bd910f
kal.radikov <14kr9@queensu.ca>
2019-10-01 10:34:06 -0600

    Add project files.

---
+++ MathGeneratorREST/Controllers/MathProblemController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MathGeneratorREST.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MathGeneratorREST.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MathProblemController : ControllerBase
+    {
+        private readonly IMathService _mathService;
+
+        public MathProblemController(IMathService mathService)
+        {
+            _mathService = mathService;
+        }
+
+        // GET: api/MathProblem
+        [HttpGet]
+        public string Get()
+        {
+            return _mathService.Generate(4,2,null);
+        }
+
+        // GET: api/MathProblem/5
+        [HttpGet("{numProblems}/{min?}/{max?}", Name = "Get")]
+        public IActionResult Get(int numProblems, int? min, int? max)
+        {
+            var data = _mathService.Generate(numProblems, min, max);
+
+            if (data != null)
+                return Ok(data);
+            return NotFound();
+        }
+
+        // POST: api/MathProblem
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/MathProblem/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ MathGeneratorREST/Services/MathService.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+
+namespace MathGeneratorREST.Services
+{
+    public class MathService : IMathService
+    {
+        private readonly List<char> _operationsList;
+        private const int DEFAULT_NUM_OPERATIONS = 10;
+
+        public MathService()
+        {
+            _operationsList = new List<char>
+            {
+                '+',
+                '-',
+                '/',
+                '*'
+            };
+        }
+
+        public string Generate(int numProblems, int? minOperations, int? maxOperations)
+        {
+            if (!ValidateInputs(numProblems, minOperations, maxOperations, out var result))
+                return result;
+
+            string problems = string.Empty;
+
+            for (int i = 0; i < numProblems; i++)
+            {
+                var operationsArray = GenerateOperationsArray(minOperations, maxOperations);
+                var numbersArray = GenerateNumbersArray(operationsArray.Count + 1);
+                var expression = MergeArrays(operationsArray, numbersArray);
+                expression += "\n";
+
+                problems += expression;
+
+            }
+
+            return problems;
+        }
+
+        private bool ValidateInputs(in int numProblems, in int? minOperations, in int? maxOperations, out string result)
+        {
+            if (numProblems == 0)
+            {
+                result = "No problems to generate";
+                return false;
+            }
+
+            //validate min operations
+            if (minOperations.HasValue)
+            {
+                if (minOperations.Value < 0 || minOperations.Value > _operationsList.Count)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(minOperations));
+                }
+            }
+
+            //validate max operations
+            if (maxOperations.HasValue)
+            {
+                if (maxOperations.Value < 0 || maxOperations.Value > _operationsList.Count)
+                    throw new ArgumentOutOfRangeException(nameof(maxOperations));
+            }
+
+            //check if min > max
+            if (maxOperations.HasValue && minOperations.HasValue)
+                if (minOperations > maxOperations)
+                    throw new ArgumentException("Min number of operations cannot be less than max");
+
+            //all checks pass
+            result = String.Empty;
+            return true;
+
+
+        }
+
+        private string MergeArrays(List<char> operations, List<int> numbers)
+        {
+            string expression = $"{numbers.FirstOrDefault()} ";
+
+            for (int i = 0; i < operations.Count; i++)
+            {
+                expression += operations[i] + " ";
+                expression += numbers[i] + " ";
+            }
+
+            return expression;
+        }
+        private List<char> GenerateOperationsArray(int? minOperations, int? maxOperations)
+        {
+            Random rng = new Random();
+            var operationsArray = new List<char>();
+            var usedOperations = new List<char>();
+            var validateFlag = false;
+
+            //regenerate new problem until it meets conditions
+            while (!validateFlag)
+            {
+                operationsArray.Clear();
+                usedOperations.Clear();
+
+                //difficulty set
+                var length = rng.Next(1, DEFAULT_NUM_OPERATIONS);
+
+                for (int i = 0; i < length; i++)
+                {
+                    var operation = _operationsList[rng.Next(0, _operationsList.Count)];
+                    operationsArray.Add(operation);
+                    if (!usedOperations.Contains(operation))
+                        usedOperations.Add(operation);
+
+                }
+                validateFlag = ValidateOperationsArray(minOperations, maxOperations, usedOperations);
+            }
+
+            return operationsArray;
+        }
+
+        private bool ValidateOperationsArray(int? minOperations, int? maxOperations, List<char> usedOperations)
+        {
+            var minFlag = true;
+            var maxFlag = true;
+
+            if (minOperations.HasValue)
+                if (minOperations > usedOperations.Count)
+                    minFlag = false;
+
+            if (maxOperations.HasValue)
+                if (maxOperations < usedOperations.Count)
+                    maxFlag = false;
+
+            return minFlag && maxFlag;
+        }
+
+        private List<int> GenerateNumbersArray(int size)
+        {
+            var numbersArray = new List<int>();
+
+            for (int i = 0; i < size; i++)
+                numbersArray.Add(RandomNumberGenerator.GetInt32(0, 20));
+
+            return numbersArray;
+        }
+    }
+}
